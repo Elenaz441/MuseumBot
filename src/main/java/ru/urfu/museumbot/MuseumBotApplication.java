59a836f959f28e03269d89ceda9a221c315612d5
EZ <ezasypkina03@list.ru>
2023-10-30 16:30:51 +0500

    Initial commit

---
+++ src/main/java/ru/urfu/museumbot/MuseumBotApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.museumbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseumBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/urfu/museumbot/MuseumBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.museumbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
