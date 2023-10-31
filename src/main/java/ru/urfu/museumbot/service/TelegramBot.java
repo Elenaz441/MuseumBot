9b0011b4ceb913c125300aded05ab4d2eda17128
SfBalaba <sf.balaba0170@gmail.com>
2023-11-01 02:11:04 +0500

    add menu list and release /viewUpcommingEvents

--- src/main/java/ru/urfu/museumbot/service/TelegramBot.java
+++ src/main/java/ru/urfu/museumbot/service/TelegramBot.java
@@ -1,11 +1,18 @@
 package ru.urfu.museumbot.service;
 
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import ru.urfu.museumbot.config.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.urfu.museumbot.models.Event;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>Класс реализации функционала телеграм бота</p>
@@ -16,8 +23,22 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
 
-    public TelegramBot(BotConfig config) {
+    final EventController eventData;
+
+    public TelegramBot(BotConfig config, EventController eventData) {
         this.config = config;
+        this.eventData = eventData;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get started"));
+        listOfCommands.add(new BotCommand("/help", "need help?"));
+//        listOfCommands.add(new BotCommand("/viewUpcomingEvents", "view Upcoming Events"));
+//        listOfCommands.add(new BotCommand("/signUpForEvent", "sing Up For Event"));
+        try{
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
@@ -44,6 +65,17 @@ public class TelegramBot extends TelegramLongPollingBot {
             switch (messageText) {
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, "help..");
+                    break;
+                case "/viewUpcomingEvents":
+                    for (Event listEvent : eventData.getListEvents()) {
+                        sendMessage(chatId, listEvent.toString());
+                    }
+                    break;
+                case "/signUpForEvent":
+                    sendMessage(chatId, "In developing..");
                     break;
                 default:
                     sendMessage(chatId, "Sorry, command was not recognized");
