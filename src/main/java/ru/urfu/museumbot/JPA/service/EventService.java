8623f0e2982e91f4ab8f7e09c2fd88d0ae1687b3
EZ <ezasypkina03@list.ru>
2023-11-10 19:13:07 +0500

    Add tests

--- src/main/java/ru/urfu/museumbot/JPA/service/EventService.java
+++ src/main/java/ru/urfu/museumbot/JPA/service/EventService.java
@@ -17,17 +17,21 @@ import java.util.*;
 @Service
 public class EventService {
 
-    @Autowired
-    private EventRepository eventRepository;
-
     public static final int AMOUNT_TO_ADD_DATE = 7;
+
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
 
     /**
      * <p>Получить список ближайших мероприятий</p>
      */
     public List<Event> getListEvents() {
         Instant now = new Date().toInstant();
-        Instant dateTo  = now.plus(AMOUNT_TO_ADD_DATE, ChronoUnit.DAYS);
+        Instant dateTo = now.plus(AMOUNT_TO_ADD_DATE, ChronoUnit.DAYS);
         return eventRepository.findAll().stream().filter(event -> event.getDate().
                         toInstant().isBefore(dateTo) && event.getDate().toInstant().isAfter(now))
                 .sorted(Comparator.comparing(Event::getDate))
