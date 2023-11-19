2a3958c2d01381e09613256c6e9428b7b5ee90f0
SfBalaba <sf.balaba0170@gmail.com>
2023-11-19 23:41:17 +0500

    add javaDoc

--- src/main/java/ru/urfu/museumbot/JPA/service/ExhibitService.java
+++ src/main/java/ru/urfu/museumbot/JPA/service/ExhibitService.java
@@ -6,6 +6,9 @@ import ru.urfu.museumbot.JPA.models.Event;
 import ru.urfu.museumbot.JPA.models.Exhibit;
 import ru.urfu.museumbot.JPA.repository.ExhibitRepository;
 
+/**
+ * <p>Класс для работы с данными из бд для сущности "Экспонат"</p>
+ */
 @Service
 public class ExhibitService {
     private final ExhibitRepository exhibitRepository;
@@ -13,6 +16,9 @@ public class ExhibitService {
     public ExhibitService(ExhibitRepository exhibitRepository) {
         this.exhibitRepository = exhibitRepository;
     }
+    /**
+     * <p>Получить экспонат по id</p>
+     */
     public Exhibit getExhibitById(Long id) {
         return exhibitRepository.getExhibitById(id);
     }
