6c7bc2168053caee83a8181f53a8230d19649177
SfBalaba <sf.balaba0170@gmail.com>
2023-12-11 11:54:56 +0500

    test: view_exhibit

--- src/main/java/ru/urfu/museumbot/commands/PreViewExhibitCommand.java
+++ src/main/java/ru/urfu/museumbot/commands/PreViewExhibitCommand.java
@@ -22,10 +22,11 @@ import static ru.urfu.museumbot.commands.Commands.VIEW_EXHIBIT;
 @Service
 public class PreViewExhibitCommand implements Command{
     private final UserService userService;
-
     static final String EXHIBIT_NOT_AVAILABLE = "Выставка ещё не началась. Эта команда недоступна";
     static final String CALLBACK_DATA = "ViewExhibit";
     static final String EXHIBIT_IS_AVAILABLE = "Выберите экспонат:";
+    static  final String IF_NO_EXHIBIT_BY_MUSEUM = "Извините," +
+            " в базе данных отсутствует информация об экспонатах данного музея";
     private final ExhibitService exhibitService;
 
     @Autowired
@@ -42,7 +43,11 @@ public class PreViewExhibitCommand implements Command{
         if(currentEvent.isEmpty()){
             return new Message(chatId, EXHIBIT_NOT_AVAILABLE);
         }
-        Map<Long, String> variants = exhibitService.getMuseumExhibits(currentEvent.get(0).getId())
+        List<Exhibit> exhibits = exhibitService.getMuseumExhibits(currentEvent.get(0).getId());
+        if (exhibits.isEmpty()){
+            return new Message(chatId, IF_NO_EXHIBIT_BY_MUSEUM);
+        }
+        Map<Long, String> variants = exhibits
                 .stream().collect(Collectors.toMap(Exhibit::getId, Exhibit::getTitle));
         Message message = new Message(chatId, EXHIBIT_IS_AVAILABLE);
         message.setButtonsContext(new ButtonsContext(CALLBACK_DATA, variants));
