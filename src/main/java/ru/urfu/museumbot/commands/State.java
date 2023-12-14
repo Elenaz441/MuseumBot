0b5a132010e662c427a9461e54bd2346470b4b1b
SfBalaba <sf.balaba0170@gmail.com>
2023-12-14 16:36:16 +0500

    remove static State.java

--- src/main/java/ru/urfu/museumbot/commands/State.java
+++ src/main/java/ru/urfu/museumbot/commands/State.java
@@ -1,27 +1,9 @@
 package ru.urfu.museumbot.commands;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 public enum State {
     INIT("Init"),
     RATE("Rate"),
     RATE_PREV("Rate_prev"),
     COMMENT("Comment");
-    private static final Map<String, State> ENUM_MAP;
-
-    static {
-        Map<String,State> map = new HashMap<>();
-        for (State instance : State.values()) {
-            map.put(instance.getStateString(),instance);
-        }
-        ENUM_MAP = Collections.unmodifiableMap(map);
-    }
-
-    public static State get (String stateString) {
-        return ENUM_MAP.get(stateString);
-    }
     private final String stateString;
     State(String stateString) {
         this.stateString = stateString;
