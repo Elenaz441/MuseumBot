28f9c9f4a77b4a508398bfe03486cd40fe223abc
SfBalaba <sf.balaba0170@gmail.com>
2023-12-14 20:28:20 +0500

    remove static from State.java

--- src/main/java/ru/urfu/museumbot/commands/State.java
+++ src/main/java/ru/urfu/museumbot/commands/State.java
@@ -1,13 +1,22 @@
 package ru.urfu.museumbot.commands;
+import java.util.Arrays;
 
-/**
- * Класс состояния пользователя или команды
- */
 public enum State {
     INIT("Init"),
     RATE("Rate"),
     RATE_PREV("Rate_prev"),
     COMMENT("Comment");
+
+    /**
+     * Получить состояние по идентификатору
+     */
+    public static State get(String stateString)
+    {
+        return Arrays.stream(State.values())
+                .filter(state -> state.getStateString().equals(stateString))
+                .findFirst().orElse(INIT);
+    }
+
     private final String stateString;
     State(String stateString) {
         this.stateString = stateString;
